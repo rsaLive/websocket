64a06be4dd53e6061b1549c8d58ac173f2bb4bdc
daiyubing <570956418@qq.com>
2016-10-22 23:18:55 +0800

    ok

--- socket.php
+++ socket.php
@@ -1,35 +1,52 @@
 <?php
-set_time_limit(0);
+	error_reporting(E_ALL);
+	set_time_limit(0);
+	ob_implicit_flush();
+
+	$socket=new socket('127.0.0.1','8000');
+	$socket->run();
+
 	class socket{
 		protected $hand;
-		public function  index()
+		public $soc;
+		public $socs;
+		public function  __construct($address,$port)
 		{
-			$sockets=array();
 			//建立套接字
-			$socket=socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
-			socket_set_option($socket, SOL_SOCKET , SO_REUSEADDR, 1);
-			socket_bind($socket,'127.0.0.1','8002');
-			socket_listen($socket);
-			$sockets[]=$socket;
+			$this->soc=$this->createSocket($address,$port);
+			$this->socs=array($this->soc);
 
+		}
+		public function createSocket()
+		{
+			$socket= socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+	        socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
+	        socket_bind($socket, '127.0.0.1','8000');
+	        socket_listen($socket);
+	        return $socket;
+		}
 
+		public function run(){
 			while(true){
-				$write=$except=null;
-				socket_select($sockets, $write, $except, null);
-				foreach($sockets as $k=>$v){
-					if($socket == $v){
-						$client=socket_accept($socket);
+				$arr=$this->socs;
+				$write=$except=NULL;
+				socket_select($arr,$write,$except, NULL);
+				foreach($arr as $k=>$v){
+					if($this->soc == $v){
+						$client=socket_accept($this->soc);
 						if($client <0){
 							echo "socket_accept() failed";
 						}else{
-							array_push($sockets,$client);
+							// array_push($this->socs,$client);
+							// unset($this[]);
+							$this->socs[]=$client;
 						}
 					}else{
 						//从已连接的socket接收数据  返回的是从socket中接收的字节数
 						$byte=socket_recv($v, $buff,20480, 0);
 						//如果接收的字节是0 返回
-						if($byte==0)
-							return;
+						if($byte<7)
+							continue;
 						//判断有没有握手没有握手则进行握手,如果握手了 则进行处理
 						if(!$this->hand[(int)$client]){
 							//进行握手操作
@@ -38,21 +55,11 @@ set_time_limit(0);
 					        $key  = trim(substr($buf,0,strpos($buf,"\r\n")));
 					     
 					        $new_key = base64_encode(sha1($key."258EAFA5-E914-47DA-95CA-C5AB0DC85B11",true));
-
-							// preg_match("/Sec-WebSocket-Key:(.*)\r\n/",$match);
-							// $key=$match[1];
-							// $new_key=base64_encode(sha1($key.'258EAFA5-E914-47DA-95CA-C5AB0DC85B11'),true);
-
 							$new_message = "HTTP/1.1 101 Switching Protocols\r\n";
 					        $new_message .= "Upgrade: websocket\r\n";
 					        $new_message .= "Sec-WebSocket-Version: 13\r\n";
 					        $new_message .= "Connection: Upgrade\r\n";
 					        $new_message .= "Sec-WebSocket-Accept: " . $new_key . "\r\n\r\n";
-					         // $upgrade = "HTTP/1.1 101 Switching Protocols\r\n" .
-				          //      "Upgrade: websocket\r\n" .
-				          //      "Connection: Upgrade\r\n" .
-				          //      "Sec-WebSocket-Accept: " . $acceptKey . "\r\n" .
-				          //      "\r\n";
 					        socket_write($v,$new_message,strlen($new_message));
 					        // socket_write(socket,$upgrade.chr(0), strlen($upgrade.chr(0)));
 					        $this->hand[(int)$client]=true;
@@ -79,25 +86,19 @@ set_time_limit(0);
 					                $data .= chr($mask[$n%4]^hexdec(substr($msg[1],$i,2)));  
 					                $n++;  
 					            }
-
 					            //发送数据到客户端
 					           	//如果长度大于125 将数据分块
 					           	$block=str_split($data,125);
 					           	$writes ="\x81".chr(strlen($block[0])).$block[0];
-					           	socket_write($v,$writes,strlen($writes));
-					           	// foreach ($block as $keys => $values) {
-					           	// 	$data='\x81'.chr(strlen($values)).$values;
-					           	// 	socket_write($v,$data);
-					           	// }
+					           	foreach ($this->socs as $key => $value) {
+					           		if($this->hand[(int)$value])
+					           			socket_write($value,$writes,strlen($writes));
+					           	}
 					        }
 						}
 					}
 				}
 			}
-			echo $socket;
 		}
 		
 	}
-
-	$socket=new socket();
-	$socket->index();
